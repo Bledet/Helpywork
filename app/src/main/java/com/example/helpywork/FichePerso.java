82d5fa0fa25590024fd66574a5e5cff886052fb6
ArthurDetant <39328708+ArthurDetant@users.noreply.github.com>
2020-03-04 23:19:31 +0100

    test

--- app/src/main/java/com/example/helpywork/FichePerso.java
+++ app/src/main/java/com/example/helpywork/FichePerso.java
@@ -9,6 +9,6 @@ public class FichePerso extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_fiche_perso);
+        setContentView(R.layout.activity_f);
     }
 }
